a801a7d0b287d8af6b9a735278ac5d35a7443254
prasum shrestha <prasumshrestha8877@gmail.com>
2026-04-28 20:02:52 +0545

    Now it's time to make levels by changing the controls it's gonna be creepy :)))

---
+++ Assets/Scripts/Controls/OppositePlayer.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using System;
+using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
+using Unity.VisualScripting;
+
+public class OppositePlayer : MonoBehaviour
+{
+    private bool isGrounded;
+
+    private bool isRunning;
+}
